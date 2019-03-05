d417bd8c2299428fe6855da01eb7afe82cd17351
Chris Waldron <chriswaldron15@gmail.com>
2019-03-05 20:13:30 +0000

    Fading UI

---
+++ Assets/Scripts/UI/Fading Menu/FadingMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Curveball;
+using TMPro;
+using DG.Tweening;
+
+namespace LastStand
+{
+    public class FadingMenu : MenuScreen
+    {
+        public bool IsFirstScreen;
+        public FadingMenuTextElement[] TextInstances;
+        public Button[] Buttons;
+
+        public override void Open(bool instant)
+        {
+            DisableButtons();
+            SetActive(true);
+
+            foreach (FadingMenuTextElement textInstance in TextInstances)
+            {
+                textInstance.FadeIn();
+            }
+
+            Timer.CreateTimer(gameObject, FadingMenuTextElement.FADE_TIME + 0.1f, EnableButtons);
+        }
+
+        public override void Close(bool instant, MenuScreen toOpen = null)
+        {
+            DisableButtons();
+
+            foreach (FadingMenuTextElement textInstance in TextInstances)
+            {
+                textInstance.FadeOut();
+            }
+
+            Timer.CreateTimer(gameObject, FadingMenuTextElement.FADE_TIME, () =>
+            {
+                SetActive(false);
+            });
+        }
+
+        private void EnableButtons()
+        {
+            foreach (Button b in Buttons)
+            {
+                b.interactable = true;
+            }
+        }
+
+        private void DisableButtons()
+        {
+            foreach (Button b in Buttons)
+            {
+                b.interactable = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Fading Menu/FadingMenuTextElement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+namespace LastStand
+{
+    [System.Serializable]
+    public class FadingMenuTextElement
+    {
+        public const float FADE_TIME = 0.425f;
+        private static readonly Vector2 MOVE_OFFSET = new Vector3(-35f, 0f);
+
+        public TextMeshProUGUI TextInstance;
+
+        private Vector2 startPosition;
+        private Vector2 fromPosition;
+        private Color startColor;
+
+        private bool initialised;
+
+        public void FadeIn()
+        {
+            Initialise();
+
+            TextInstance.color = new Color(startColor.r, startColor.g, startColor.b, 0f);
+            TextInstance.rectTransform.anchoredPosition = fromPosition;
+
+            TextInstance.DOFade(1f, FADE_TIME).SetDelay(FADE_TIME);
+            TextInstance.rectTransform.DOAnchorPos(startPosition, FADE_TIME).SetDelay(FADE_TIME);
+        }
+
+        public void FadeOut()
+        {
+            Initialise();
+
+            TextInstance.color = new Color(startColor.r, startColor.g, startColor.b, 1f);
+            TextInstance.rectTransform.anchoredPosition = startPosition;
+
+            TextInstance.DOFade(0f, FADE_TIME);
+            TextInstance.rectTransform.DOAnchorPos(fromPosition, FADE_TIME);
+        }
+
+        private void Initialise()
+        {
+            if (initialised)
+                return;
+
+            startPosition = TextInstance.rectTransform.anchoredPosition;
+            fromPosition = startPosition + MOVE_OFFSET;
+            startColor = TextInstance.color;
+
+            initialised = true;
+        }
+    }
+}
